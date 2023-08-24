d21812d5c62f0faab55252ff81d799142614e563
shivamkumarjha711 <shivamkumarjha711@gmail.com>
2023-08-25 00:34:25 +0530

    product api created

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,41 @@
 import express from 'express';
 import { requireSignIn, isAdmin } from '../middlewares/authMiddleware.js'
-import { createProductController } from '../controllers/productController.js';
+import { createProductController, deleteProductController, getProductController, getSingleProductController, productPhotoController, updateProductController } from '../controllers/productController.js';
+import formidable from 'express-formidable';
 
 const router = express.Router();
 
 // routes
-router.post('/create-product', requireSignIn, isAdmin, createProductController)
+router.post(
+    '/create-product', 
+    requireSignIn, 
+    isAdmin, 
+    formidable(),
+    createProductController
+    )
+
+// update product
+router.put(
+    '/update-product/:pid', 
+    requireSignIn, 
+    isAdmin, 
+    formidable(),
+    updateProductController
+    )
+
+// get products
+router.get('/get-product', getProductController)
+
+// single product
+router.get('/get-product/:slug', getSingleProductController)
+
+// get photo
+router.get('/product-photo/:pid', productPhotoController)
+
+// delete product
+router.delete(
+    '/delet-product/:pid', 
+    deleteProductController
+    )
 
 export default router;
